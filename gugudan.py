a345c0e47501e7c5869eff7694b977e63182293d
ParkJunSeok91 <nicecnun@kookmin.ac.kr>
2015-08-26 01:28:52 +0900

    GuGuDan print

---
+++ gugudan.py
@@ -0,0 +1,17 @@
+__author__ = 'user'
+
+for x in range(1,10):
+    print(' ')
+    for y in range(2,5):
+            print('%d X %d = %2d' %(y,x,x*y), end="  ")
+print(' ')
+for x in range(1,10):
+    print(' ')
+    for y in range(5,8):
+            print('%d X %d = %2d' %(y,x,x*y), end="  ")
+print(' ')
+for x in range(1,10):
+    print(' ')
+    for y in range(8,10):
+            print('%d X %d = %2d' %(y,x,x*y), end="  ")
+print(' ')
